383c8c6328fcc1317521cdff2b6fa30e00bb798e
Kateryna Semyroz <kateryna.semyroz@gmail.com>
2017-08-08 17:08:31 +0200

    Fixed structure of component

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import styles from './styles.scss';
 
-class Login extends React.Component {  
+class Login extends React.Component { 
+	constructor(){
+        super();
+        this.handleLogIn = this.handleLogIn.bind(this);
+    }
+
+    handleLogIn(event){
+    	console.log("Button was clicked");
+    }
+
     render() {
         return (
         	<div>
         		<p>Sing into your account</p>
         		<p>Enter your email</p>
-        		<input placeholder="Email" ref="theInput"></input>
+        		<input placeholder="Email" ref="theInput"/>
         		<p>Enter your password</p>
-        		<input placeholder="Password" ref="theInput"></input>
+        		<input placeholder="Password" ref="theInput"/><br/>
+        		<input type="checkbox" id="rememberMe"/>
+        		<label htmlFor="rememberMe">Remember me</label><br/>
+        		<button onClick={this.handleLogIn}>Remove User</button>
         	</div>
         )
     }

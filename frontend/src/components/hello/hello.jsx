6568b56231d190eabdc49b07713d910dc79b4d07
DenysKuts <crudelis2@gmail.com>
2017-08-09 21:36:07 +0300

    deleted unnecessary markup for testing in hello component

--- frontend/src/components/hello/hello.jsx
+++ frontend/src/components/hello/hello.jsx
@@ -5,54 +5,11 @@ import styles from './styles.scss';
 import * as usersActions from '../../actions/usersActions';
 
 class Hello extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('codename', this.input.files[0]);
-    const options = {
-      body: formData,
-      method: 'POST',
-    };
-    fetch('http://localhost:3000/api/uploads', options)
-      .then((response) => {
-        console.log('data sent succesfully');
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(`error has occurred ${err}`);
-      });
-  }
-
   render() {
     return (
       <div>
         <div className={styles['project-name']}>Insight</div>
-        <button
-          onClick={() => {
-            this.props.getAllUsers();
-          }}
-        > GET USERS
-        </button>
-        <form>
-          <input
-            name="fileToUpload"
-            type="file"
-            ref={(node) => {
-              this.input = node;
-            }}
-          />
-          <button type="submit" onClick={this.onSubmit}>
-            Send File
-          </button>
-        </form>
+        <button onClick={() => this.props.getAllUsers()}>GET USERS</button>
       </div>
     );
   }

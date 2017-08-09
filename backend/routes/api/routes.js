afaea4d6d13dfcb804dd5d370dd2b15a6ff32802
DenysKuts <crudelis2@gmail.com>
2017-08-09 21:31:15 +0300

    add files upload route

--- backend/routes/api/routes.js
+++ backend/routes/api/routes.js
@@ -1,7 +1,9 @@
 const userRoutes = require('./userRoutes');
+const uploadRoutes = require('./uploadRoutes');
 
 module.exports = function (app) {
-    return {
-        userRoutes: userRoutes(app)
-    };
+  return {
+    userRoutes: userRoutes(app),
+    uploadRoutes: uploadRoutes(app),
+  };
 };
--- frontend/src/components/hello/hello.jsx
+++ frontend/src/components/hello/hello.jsx
@@ -5,11 +5,54 @@ import styles from './styles.scss';
 import * as usersActions from '../../actions/usersActions';
 
 class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('codename', this.input.files[0]);
+    const options = {
+      body: formData,
+      method: 'POST',
+    };
+    fetch('http://localhost:3000/api/uploads', options)
+      .then((response) => {
+        console.log('data sent succesfully');
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(`error has occurred ${err}`);
+      });
+  }
+
   render() {
     return (
       <div>
         <div className={styles['project-name']}>Insight</div>
-        <button onClick={() => { this.props.getAllUsers(); }}>GET USERS</button>
+        <button
+          onClick={() => {
+            this.props.getAllUsers();
+          }}
+        > GET USERS
+        </button>
+        <form>
+          <input
+            name="fileToUpload"
+            type="file"
+            ref={(node) => {
+              this.input = node;
+            }}
+          />
+          <button type="submit" onClick={this.onSubmit}>
+            Send File
+          </button>
+        </form>
       </div>
     );
   }
@@ -19,13 +62,13 @@ Hello.propTypes = {
   getAllUsers: PropTypes.func,
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     users: state.users,
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     getAllUsers: () => {
       dispatch(usersActions.getAllUsers());

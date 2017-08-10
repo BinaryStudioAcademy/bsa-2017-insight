69dce131c4610251f541f8608df9f84efb2a58b2
DenysKuts <crudelis2@gmail.com>
2017-08-10 16:14:36 +0300

    simple chatWidget component for routes testing

---
+++ frontend/src/components/chatWidget/chatWidget.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import './chatWidget.css';
+
+class ChatWidget extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>This is chat</div>
+    );
+  }
+}
+
+export default ChatWidget;

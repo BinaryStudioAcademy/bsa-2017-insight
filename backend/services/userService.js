ac3b06f5315484d44be059c883b55f031f92d1ea
DenysKuts <crudelis2@gmail.com>
2017-08-11 23:50:30 +0300

    add userService for user and user-statistics simultaneous creation

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,35 +1,26 @@
-const async = require('async');
-const userRepository = require('../repositories/visitorRepository');
+const userRepository = require('../repositories/userRepository');
+const statisticsRepository = require('../repositories/statisticsRepository');
+const bcrypt = require('bcrypt-nodejs');
 
-module.exports = function(id, onResult) {
-  let resData = {};
-  async.waterfall(
-    [
-      function (callback) {
-        userRepository.add(id, (err, data) => {
-          if (err) {
-            return callback(err, data);
-          }
-          if (data) {
-            resData.features = data.features;
-            callback(null, resData);
-          }
+function createUser(id, userData, callback) {
+  if (id) {
+    userRepository.addAnonymousUser(id).then((data) => {
+      callback(data);
+    });
+  } else {
+    userData.salt = bcrypt.genSaltSync(10);
+    bcrypt.hash(userData.password, userData.salt, null, (err, hash) => {
+      userData.password = hash;
+      userRepository.model.create(userData).then((newUserData) => {
+        console.log(newUserData);
+        const statisticsObj = { userId: newUserData._id };
+        statisticsRepository.model.create(statisticsObj).then((statisticsData) => {
+          console.log(statisticsData);
+          callback(userData);
         });
-      },
-      function (data, callback) {
-        userRepository.add(id, (err, data) => {
-          if (err) {
-            return callback(err, data);
-          }
-          if (data) {
-            resData.features = data.features;
-            callback(null, resData);
-          }
-        });
-      },
-    ],
-    (err, result) => {
-      onResult(err, result);
-    },
-  );
-};
+      });
+    });
+  }
+}
+
+module.exports = createUser;

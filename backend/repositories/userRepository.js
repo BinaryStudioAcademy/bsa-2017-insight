f6a89831bfc2df627814c7c2b8f3a68095aa6913
Denis Terekhov <den.terekhov@ukr.net>
2017-08-11 11:52:40 +0300

    save dev before merge with II-2

--- backend/repositories/userRepository.js
+++ backend/repositories/userRepository.js
@@ -1,24 +1,28 @@
 var connection = require('../db/dbConnect');
 var Repository = require('./generalRepository');
 var User = require('../schemas/userSchema');
+var bcrypt = require('bcrypt-nodejs');
 
 function UserRepository() {
-	Repository.prototype.constructor.call(this);
-	this.model = User;
+  Repository.prototype.constructor.call(this);
+  this.model = User;
 }
 
 UserRepository.prototype = new Repository();
 
-UserRepository.prototype.oneMoreFunction = function(userId, obj, callback) {
-	var model = this.model;
-	var query = model.findByIdAndUpdate(userId, {
-		$push: {
-			property:{
-				nestedProperty: value,
-			}
-		}
-	}, {});
-	query.exec(callback);
+UserRepository.prototype.getUserByName = function(username, callback) {
+  var model = this.model;
+  var query = model.findOne({ 'username':  username });
+  query.exec(callback);
+};
+
+UserRepository.prototype.add = function(data, callback) {
+  var self = this;
+  data.salt = bcrypt.genSaltSync(10);
+  bcrypt.hash(data.password, data.salt, null, function(err, hash) {
+    data.password = hash;
+    Repository.prototype.add.call(self, data, callback);
+  });
 };
 
 module.exports = new UserRepository();

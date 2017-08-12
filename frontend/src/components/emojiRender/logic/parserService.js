418ddccdef1b03648fd01bb03e705255a0489fcc
yurkkaa <s.yuri@ukr.net>
2017-08-12 17:44:15 +0300

    separate logic and settings

--- frontend/src/components/emojiRender/logic/parserService.js
+++ frontend/src/components/emojiRender/logic/parserService.js
@@ -1,24 +1,15 @@
-import map from "../map.json"
+
 import React from "react";   
+import * as setups from "../settings"
 
-const  link = "https://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/"
-const categories = {}
-
-
-
-categories.people  = setCategory("people")
-categories.nature  = setCategory("nature")
-categories.objects = setCategory("objects")             
-categories.places  = setCategory("places")
-categories.symbols = setCategory("symbols") 
-
+const categories = setCategory(setups.categories)
 
 function get(stringToParse){
     let lastChar = stringToParse.length
     let indexes = []
     let itemsToRender = []
     let result  = null
-    let regExp = /:\S+[^:]+:/g
+    let regExp = /:\S[^:]+:/g
 
     while(result = regExp.exec(stringToParse)){
         indexes.push(result.index,regExp.lastIndex)
@@ -31,36 +22,42 @@ function get(stringToParse){
     if(indexes[indexes.length-1] !== lastChar){
         indexes.push(lastChar)
     }
-      
+        console.log(indexes)
     indexes.reduce((prev,curr)=>{
         itemsToRender.push(stringToParse.slice(prev,curr)) 
         return prev = curr
     })  
-       
+        console.log(itemsToRender)
     let newItemsToRender = itemsToRender.map((e)=>{
         let search = e.trim()  
-        let firstCond  = /:\S+[^:]+:/g.test(search)
-        let secondCond  =  map[search] ? map[search]["status"] : false
+        let firstCond  = /:\S[^:]+:/g.test(search)
+        let secondCond  =  setups.map[search] ? setups.map[search]["status"] : false
 
         if(firstCond  && !secondCond ){
              return  trim(search) 
         } else return  search
             
     })
-       
+        console.log(newItemsToRender)
         return getParseString(newItemsToRender)
     }
 
     function category(category){
         category = category.toLowerCase()
         let categoryExist = categories[category] ? categories[category] : false
-        return categoryExist ? getParseString(categoryExist) : getParseString(Object.keys(map))
+        return categoryExist ? getParseString(categoryExist) : getParseString(Object.keys(setups.map))
     }
 
-    function setCategory(category){
-        return  Object.keys(map).filter((e)=>{
-                  return  map[e]["category"] === category
+    function setCategory(arr){
+       
+        const cat = {}
+        arr.forEach(function(category) {
+            cat[category] = Object.keys(setups.map).filter((e)=>{
+                  return  setups.map[e]["category"] === category
                 })
+        });
+           
+        return cat
     }
 
     function trim(string){
@@ -78,15 +75,15 @@ function get(stringToParse){
            height: "20px",
            display: "inline-block",
            backgroundSize: "20px 20px",
-           backgroundImage: `url(${link}${emoji}.png)`
+           backgroundImage: `url(${setups.link}${emoji}.png)`
         }
     }
 
     function  getParseString(res){
        
-         return  res.map((e)=>{
+        return  res.map((e)=>{
           
-            if(/:\S+[^:]+:/.test(e) || /:\S+:/.test(e)){
+            if(/:\S[^:]+:/.test(e) || /:\S+:/.test(e)){
                        
                 let key = (Math.random().toString(16).slice(2) + Math.random().toString(16).slice(2)).slice(0,24) 
                
@@ -99,6 +96,9 @@ function get(stringToParse){
             return  e
 
          })
+       
      }
+
+    
 
 export {get,category}
---
+++ frontend/src/components/emojiRender/settings/index.js
@@ -0,0 +1,12 @@
+import map from "./map.json"
+const link = "https://www.webpagefx.com/tools/emoji-cheat-sheet/graphics/emojis/"
+const categories = [
+    "people",
+    "nature",
+    "objects",
+    "places",
+    "symbols",
+]
+
+
+export {link,categories,map}

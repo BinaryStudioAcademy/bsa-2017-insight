3e413a100330461b0c32ba9068b428b5468bda53
yurkkaa <s.yuri@ukr.net>
2017-08-15 11:13:13 +0300

    add conversationSaga to the rootSaga and combine reducers

--- frontend/src/saga/rootSaga.js
+++ frontend/src/saga/rootSaga.js
@@ -1,8 +1,10 @@
 import { fork } from 'redux-saga/effects';
 import usersSaga from './usersSaga';
+import conversationsSaga from './conversationsSaga';
 
 function* rootSaga() {
   yield [fork(usersSaga)];
+  yield [fork(conversationsSaga)];
 }
 
 export default rootSaga;

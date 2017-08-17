43bd1a3bd3ba8cc86c59f7b8a736825ab460998a
DenysKuts <crudelis2@gmail.com>
2017-08-17 13:25:56 +0300

    small improvements

--- frontend/src/components/admin/Respond/SingleConversation/index.jsx
+++ frontend/src/components/admin/Respond/SingleConversation/index.jsx
@@ -1,34 +1,34 @@
 import React from "react"
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
-import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
+import { grey400, darkBlack, lightBlack } from 'material-ui/styles/colors';
 import MyThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const SingleConversation = (props) =>{
-    console.log(props.conversation)
-    let messages = props.conversation.messages
-   return (
-       <div>
-           <MyThemeProvider>
+const SingleConversation = (props) => {
+  console.log(props.conversation)
+  let messages = props.conversation.messages
+  return (
+    <div>
+      <MyThemeProvider>
         <List>
-            <Subheader>Today</Subheader>
-            <ListItem
+          <Subheader>Today</Subheader>
+          <ListItem
             leftAvatar={<Avatar src="images/ok-128.jpg" />}
             primaryText={props.conversation._id}
             secondaryText={
-                <p>
-          
-                </p>
+              <p>
+
+              </p>
             }
             secondaryTextLines={2}
-            />
-            <Divider inset={true} />
+          />
+          <Divider inset={true} />
         </List>
-        </MyThemeProvider>
-       </div>
-   )
+      </MyThemeProvider>
+    </div>
+  )
 }
 
 export default SingleConversation
--- frontend/src/components/analytics/getStatistics.js
+++ frontend/src/components/analytics/getStatistics.js
@@ -1,5 +1,5 @@
 export function getStatisticById(id) {
-  return fetch(`http://localhost:3000/api/statistics/${id}`)
+  return fetch(`http://localhost:3000/api/statistics/by-user/${id}`)
     .then((response) => {
       return response.json();
     })

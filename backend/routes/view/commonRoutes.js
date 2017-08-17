3830deaa4dc5d09b58414939b4e0b5d6cded8ec5
ArtemManukyan <artman0@yandex.ru>
2017-08-17 09:39:45 +0300

    Login pages integration in progress

--- backend/routes/view/commonRoutes.js
+++ backend/routes/view/commonRoutes.js
@@ -3,5 +3,7 @@ const injectData = require('../../middleware/injectedDataMiddleware');
 module.exports = function (app) {
   app.get('*', (req, res) => {
     injectData(req, res, {}, false);
+    console.log('Request from: ');
+    console.log(req.user);
   });
 };

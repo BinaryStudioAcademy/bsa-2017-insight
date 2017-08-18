5254329b3048a40da496041ef76a798776a67345
ArtemManukyan <artman0@yandex.ru>
2017-08-18 18:02:35 +0300

    Validation implementing in progress: implemented for user

--- frontend/src/components/landing/UserAuthentication/UserRegistration.jsx
+++ frontend/src/components/landing/UserAuthentication/UserRegistration.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import isLength from 'validator/lib/isLength';
+import equals from 'validator/lib/equals';
+import isAfter from 'validator/lib/isAfter';
 import styles from './styles.scss';
-// import { validated } from 'react-custom-validation';
-import validator from 'validator';
 
 class UserRegistration extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      info: '',
+      info: [],
       formValues: {
         firstName: '',
         lastName: '',
         userName: '',
         password1: '',
         password2: '',
+        birstday: '',
       },
     };
     this.sendForm = this.sendForm.bind(this);
@@ -25,19 +27,48 @@ class UserRegistration extends React.Component {
         [field]: filledField.value.toString(),
       }),
     });
-    console.log(this.state);
   }
 
   formValidator() {
     const errors = [];
-    if (!validator.isLength(this.state.formValues.firstName, { min: 2 })) {
-      errors.push('Please enter your first name (it should be longer than 2 letters)');
+    // First name's length > 2
+    if (!isLength(this.state.formValues.firstName, { min: 2 })) {
+      errors.push('First name should be longer than 2 symbols');
     }
+    // Last name's length > 2
+    if (!isLength(this.state.formValues.lastName, { min: 2 })) {
+      errors.push('Last name should be longer than 2 symbols');
+    }
+    // Username's length > 3 && < 15
+    if (!isLength(this.state.formValues.userName, { min: 3, max: 15 })) {
+      errors.push('Username should be from 3 to 15 symbols long');
+    }
+    // Matching of password field values
+    if (!equals(this.state.formValues.password2, this.state.formValues.password1)) {
+      errors.push('Passwords don\'t match');
+    }
+    // Password's length > 6
+    if (!isLength(this.state.formValues.password2, { min: 6, max: 30 })) {
+      errors.push('Password should be from 6 to 30 symbols long');
+    }
+    // Passsword containing a letter and a digit
+    if (!/[a-z]/i.test(this.state.formValues.password2)
+        || !/\d/.test(this.state.formValues.password2)) {
+      errors.push('Password should contain both letters and digits');
+    }
+    // Birstday is not in the future
+    if (isAfter(this.state.formValues.birstday)) {
+      errors.push('Are you sure that you were born in future?');
+    }
+    return errors;
   }
 
   sendForm(e) {
     e.preventDefault();
-    this.formValidator();
+
+    this.setState({ info: this.formValidator() });
+    if (this.state.info.length) return;
+
     const formData = new FormData(e.target);
     fetch('/api/user/registration/', {
       method: 'POST',
@@ -45,7 +76,7 @@ class UserRegistration extends React.Component {
       redirect: 'follow',
       credentials: 'include',
     }).then(response => response.json()).then((response) => {
-      this.setState({ info: response.text });
+      this.setState({ info: [response.text] });
     });
   }
 
@@ -56,7 +87,7 @@ class UserRegistration extends React.Component {
         <hr />
         <form onSubmit={this.sendForm} encType="multipart/form-data">
           <div className={styles['get-data']}>
-            <span>First Name*</span>
+            <span>First Name</span>
             <input
               type="text"
               name="firstName"
@@ -65,7 +96,7 @@ class UserRegistration extends React.Component {
             />
           </div>
           <div className={styles['get-data']}>
-            <span>Last Name*</span>
+            <span>Last Name</span>
             <input
               type="text"
               name="lastName"
@@ -74,22 +105,25 @@ class UserRegistration extends React.Component {
             />
           </div>
           <div className={styles['get-data']}>
-            <span>Date of birth*</span>
+            <span>Date of birth</span>
             <input
               type="date"
               name="dateOfBirth"
               required
+              onChange={e => this.formValuesSaver('birstday', e.target)}
             />
           </div>
           <div className={styles['get-data']}>
             <span>Gender</span>
             <div>
-              <input type="radio" name="gender" value="Male" id="male" /><label htmlFor="male">Male</label>
-              <input type="radio" name="gender" value="Female" id="female" /><label htmlFor="female">Female</label>
+              <input type="radio" required name="gender" value="Male" id="male" />
+              <label htmlFor="male">Male</label>
+              <input type="radio" required name="gender" value="Female" id="female" />
+              <label htmlFor="female">Female</label>
             </div>
           </div>
           <div className={styles['get-data']}>
-            <span>Username*</span>
+            <span>Username</span>
             <input
               type="text"
               name="username"
@@ -98,11 +132,11 @@ class UserRegistration extends React.Component {
             />
           </div>
           <div className={styles['get-data']}>
-            <span>Email*</span>
+            <span>Email</span>
             <input type="email" name="email" required />
           </div>
           <div className={styles['get-data']}>
-            <span>Password*</span>
+            <span>Password</span>
             <input
               type="password"
               name="firstPassword"
@@ -111,7 +145,7 @@ class UserRegistration extends React.Component {
             />
           </div>
           <div className={styles['get-data']}>
-            <span>Confirm password*</span>
+            <span>Confirm password</span>
             <input
               type="password"
               name="secondPassword"
@@ -121,7 +155,7 @@ class UserRegistration extends React.Component {
           </div>
           <div className={styles['get-data']}>
             <span>Company</span>
-            <input type="text" name="company" />
+            <input type="text" name="company" required />
           </div>
           <div className={styles['get-data']}>
             <span>Avatar</span>
@@ -130,7 +164,11 @@ class UserRegistration extends React.Component {
           <button type="submit">Sign Up</button>
         </form>
         <br />
-        <div className={styles['error-message']}>{this.state.info}</div>
+        <div className={styles['error-message']}>
+          {this.state.info.map((err) => {
+            return (<span>{err}<br /></span>);
+          })}
+        </div>
       </div>
     );
   }

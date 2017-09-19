43ba071930d6ce427ce6479e88abea2504edfca2
ArtemManukyan <artman0@yandex.ru>
2017-09-19 19:41:44 +0300

    Landing is filled

--- frontend/src/components/landing/Resources/HelpCenter.jsx
+++ frontend/src/components/landing/Resources/HelpCenter.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 class HelpCenter extends React.Component {
   render() {
     return (
-      <div>
-        Help Center
+      <div style={{ padding: '150px 20px' }}>
+        <h2>Emmm... help center?</h2>
+        <p>See this button in right bottom corner? Just click on it right now! See? You can ask anything via
+ this chat. Moreover, it&#39;s a great opportunity to try one of our products.</p>
       </div>
     );
   }

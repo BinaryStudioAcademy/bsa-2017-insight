86f9795fee5cf942e91e445e41bc0f1105447b9d
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2017-09-21 09:31:06 +0300

    fix lint errors

--- frontend/src/components/admin/Table/TableItself.jsx
+++ frontend/src/components/admin/Table/TableItself.jsx
@@ -151,13 +151,13 @@ class TableItself extends React.Component {
                   />
                 </th>
                 {this.props.options.map((elem) => {
-                  if( elem === 'browser' || elem === 'country' || elem === 'os') {
-                    return (<th
-                    key={elem}
-                    className={styles['text-center']}
-                  >
-                    {this.state[elem]}
-                  </th>);
+                    if (elem === 'browser' || elem === 'country' || elem === 'os') {
+                      return (<th
+                      key={elem}
+                      className={styles['text-center']}
+                    >
+                      {this.state[elem]}
+                    </th>);
                   }
                   return (<th
                     key={elem}
